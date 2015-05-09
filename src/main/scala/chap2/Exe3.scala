c1d62db244ef69bdd7273c369693ffa872988b46
ponkotuy <web@ponkotuy.com>
2015-05-10 01:14:10 +0900

    Add chap2.Exe4

--- src/main/scala/chap2/Exe3.scala
+++ src/main/scala/chap2/Exe3.scala
@@ -1,6 +1,6 @@
 package chap2
 
-object Exe3 extends App {
+object Exe3 {
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
     (a: A) => {
       (b: B) => f(a, b)
---
+++ src/main/scala/chap2/Exe4.scala
@@ -0,0 +1,6 @@
+package chap2
+
+object Exe4 {
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b)
+}
