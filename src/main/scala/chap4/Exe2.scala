6a03e3dedd165c549d5b61349cb02baca1689ca6
ponkotuy <web@ponkotuy.com>
2015-06-04 21:15:10 +0900

    Refactor chap4.Exe2

--- src/main/scala/chap4/Exe2.scala
+++ src/main/scala/chap4/Exe2.scala
@@ -1,21 +1,19 @@
 package chap4
 
 object Exe2 extends App {
+  def sumOpt(xs: Seq[Option[Double]]): Option[Double] = xs.reduce { (optX, optY) =>
+    for {
+      x <- optX
+      y <- optY
+    } yield x + y
+  }
+
   def variance(xs: Seq[Double]): Option[Double] = {
-    val opts: Seq[Option[Double]] = xs.map { x => if(x.isNaN) None else Some(x) }
-    val average = opts.reduce { (optX, optY) =>
-      for {
-        x <- optX
-        y <- optY
-      } yield x + y
-    }.map(_ / opts.size)
+    val opts = xs.map { x => if(x.isNaN) None else Some(x) }
+    val average = sumOpt(opts).map(_ / opts.size)
     average.flatMap { ave =>
-      opts.map(_.map { x => math.pow(x - ave, 2) }).reduce { (optX, optY) =>
-        for {
-          x <- optX
-          y <- optY
-        } yield x + y
-      }.map(_ / opts.size)
+      val vs = opts.map(_.map { x => math.pow(x - ave, 2) })
+      sumOpt(vs).map(_ / opts.size)
     }
   }
 
